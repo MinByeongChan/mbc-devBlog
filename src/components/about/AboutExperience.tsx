b2424cf14d31938079535ba13ceb099ce928f45e
MinByeongChan <mbc0481@naver.com>
2024-08-24 23:02:23 +0900

    feat(about): IntersectionObserver 텍스트 전환

--- src/components/about/AboutExperience.tsx
+++ src/components/about/AboutExperience.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import TextDefault from '../ui/TextDefault';
-import { fontSize, fontWeight } from '@/utils/StyleTheme';
+import { fontWeight } from '@/utils/StyleTheme';
 import Image from 'next/image';
 
 const ListItems = styled.ul`
