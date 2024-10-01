167a1b4f56707bfb95f075ef59de0dac73e314a9
MinByeongChan <mbc0481@naver.com>
2024-10-01 15:49:21 +0900

    feat(feature/23): Main 미사용 변수 수정

--- src/components/templates/Main.tsx
+++ src/components/templates/Main.tsx
@@ -1,71 +1,14 @@
 import React, { ReactNode } from 'react';
-
 import styled from '@emotion/styled';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelopeOpen, faPhoneSquareAlt } from '@fortawesome/free-solid-svg-icons';
-import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
-import { color, fontSize, fontWeight } from '@/utils/StyleTheme';
 import { Footer, NavigationBar } from '@/components/templates';
 
-type MainProps = {
+interface MainProps {
   meta: ReactNode;
   children: ReactNode;
-};
+}
 
 const LayoutMain = styled.main`
   min-height: 650px;
-`;
-
-const BottomWrapper = styled.footer`
-  background-color: ${color.black};
-  padding: 60px 30px;
-  color: ${color.white};
-  a {
-    color: ${color.orange};
-    font-weight: ${fontWeight.bold};
-  }
-  @media screen and (min-width: 0px) and (max-width: 480px) {
-    padding: 40px 20px;
-  }
-`;
-const BottomContainer = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-`;
-
-const BottomContentTitle = styled.div`
-  margin-bottom: 20px;
-  .footer-title {
-    margin: 0 0 10px 0;
-    font-size: ${fontSize.xg};
-    font-weight: ${fontWeight.bold};
-  }
-  .footer-subtitle {
-    margin: 0;
-    font-size: ${fontSize.sm};
-  }
-`;
-
-const BottomContentMid = styled.div`
-  .mid-container {
-    display: flex;
-    align-items: center;
-    margin: 10px 0;
-  }
-  .left-img {
-    width: 22px;
-    height: auto;
-  }
-  a {
-    font-size: ${fontSize.sm};
-    margin-left: 15px;
-  }
-`;
-
-const BottomContentEnd = styled.div`
-  margin-top: 50px;
-  text-align: center;
 `;
 
 const Main = (props: MainProps) => (
