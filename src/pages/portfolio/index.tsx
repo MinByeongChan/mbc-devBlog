bc5b88584dc5b643df5de35452f3fa3942333573
MinByeongChan <mbc0481@naver.com>
2024-12-26 08:46:10 +0900

    feat(main): warning 제거

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Meta } from '@/layout';
-import { Main } from '@/components/templates';
 import { Banner } from '@/components/portfolio/Banner';
 import { Introduction } from '@/components/portfolio/Introduction';
 import { Strength } from '@/components/portfolio/Strength';
