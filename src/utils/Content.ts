b9384886dbd69e3c6caee65c880cb46e7e7cbfa6
cw-mbc0481 <mbc0481@coway.com>
2024-12-29 22:19:23 +0900

    fix(25): 함수명 변경

--- src/utils/Content.ts
+++ src/utils/Content.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 import matter from 'gray-matter';
 import { FrontMatter, FuncPostBySlug } from '@/types';
 
-export const getPostSlugs = () => {
+export const readDirectoryFiles = () => {
   const postsDirectory = join(process.cwd(), '_posts');
   return fs.readdirSync(postsDirectory);
 };
@@ -24,6 +24,6 @@ export const getPostBySlug: FuncPostBySlug = (slug) => {
 };
 
 export function getAllPosts() {
-  const slugs = getPostSlugs();
+  const slugs = readDirectoryFiles();
   return slugs.map((slug) => getPostBySlug(slug)).sort((a, b) => (a.date > b.date ? -1 : 1));
 }
