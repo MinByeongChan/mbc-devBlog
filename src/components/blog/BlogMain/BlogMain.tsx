26426d47273b86fac45ede3c58d0f86612c9eea2
cw-mbc0481 <mbc0481@coway.com>
2025-01-01 22:58:23 +0900

    feat(25): blog main 비즈니스 로직 커스텀 훅으로 전환

--- src/components/blog/BlogMain/BlogMain.tsx
+++ src/components/blog/BlogMain/BlogMain.tsx
@@ -1,9 +1,8 @@
 import { BlogList } from '@/components/blog/BlogList/BlogList';
 import { ArticleContainer, LnbContainer } from '@/components/blog/BlogMain/BlogMain.style';
 import { LnbList } from '@/components/blog/LnbList/LnbList';
-import { BlogListItem, FrontMatter } from '@/types';
-import { getTagItems } from '@/utils/utils';
-import { useSearchParams } from 'next/navigation';
+import { useBlogMain } from '@/hooks/blog/useBlogMain';
+import { FrontMatter } from '@/types';
 import React from 'react';
 
 interface BlogMainProps {
@@ -11,23 +10,7 @@ interface BlogMainProps {
 }
 
 export const BlogMain = ({ totalPostList }: BlogMainProps) => {
-  const searchParams = useSearchParams();
-  const tag = searchParams.get('tag') || '';
-
-  const tagItems = getTagItems(totalPostList);
-  const blogListItem = totalPostList
-    .map<BlogListItem>((data) => ({
-      id: data.slug,
-      date: data.date,
-      title: data.title,
-      tags: data.tags,
-      description: data.description || data.content.substring(0, 200),
-    }))
-    .filter((data) => {
-      if (!tag) return true;
-      const lowerCaseList = data.tags?.map((data) => data.toLowerCase()) ?? [];
-      return lowerCaseList.includes(tag.toLowerCase());
-    });
+  const { tagItems, blogListItem } = useBlogMain(totalPostList);
 
   return (
     <>
