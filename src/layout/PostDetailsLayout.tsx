8e11550a4ea6464db5ca81b3b823cbeea808a8be
cw-mbc0481 <mbc0481@coway.com>
2025-01-02 08:55:17 +0900

    feat(25): posts 화면 수정

--- src/layout/PostDetailsLayout.tsx
+++ src/layout/PostDetailsLayout.tsx
@@ -5,7 +5,7 @@ interface PostDetailsLayoutProps extends PropsWithChildren {}
 
 const LayoutMain = styled.main`
   min-height: 650px;
-  grid-column: 3 / span 8;
+  grid-column: 4 / span 7;
   @container section-container (max-width: 600px) {
     &.post-details-main {
       grid-column: 2 / span 10;
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -6,63 +6,32 @@ import { Main } from '@/components/templates';
 import { getAllPosts, getPostBySlug } from '@/utils/Content';
 import { format } from 'date-fns';
 import styled from '@emotion/styled';
-import { color, fontSize, fontWeight } from '@/utils/StyleTheme';
+import { Color } from '@/utils/StyleTheme';
 import { Config } from '@/utils/Config';
 import Utterances from '@/components/comment/Utterances';
-import TextDefault from '@/components/ui/TextDefault';
 import Markdown from '@/components/Markdown';
+import { Avatar, Box, Divider, Icon, Tooltip, Typography } from '@mui/material';
+import { FrontMatter } from '@/types';
+import { Email, Instagram } from '@mui/icons-material';
+import Link from 'next/link';
 
 type PostUrl = {
   slug: string;
 };
 
-interface PostDetailsProps {
-  title: string;
-  description: string;
-  date: string;
-  modified_date: string;
-  image: string;
-  content: string;
-}
+const TitleContainer = styled(Box)({
+  marginBottom: '16px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
 
-const TitleContainer = styled.div`
-  margin-bottom: 12px;
-  .title {
-    font-size: ${fontSize.h2};
-    font-weight: ${fontWeight.bold};
-    color: ${color.darkBlack};
-  }
-  .date {
-    font-size: ${fontSize.sm};
-    font-weight: ${fontWeight.bold};
-    color: ${color.gray};
-  }
-`;
-
-const SubTitleContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding-bottom: 40px;
-  .author-img {
-    width: 30px;
-    height: 30px;
-    background-image: url('/assets/images/mbc_img.png');
-    background-position: 50% 50%;
-    background-size: cover;
-    margin-right: 10px;
-  }
-  .author-name {
-    color: ${color.darkBlack};
-    font-size: ${fontSize.md};
-    font-weight: ${fontWeight.bold};
-  }
-`;
-
-const CommentTitleContainer = styled.div`
-  margin: 50px 0 20px 0;
-  padding-bottom: 20px;
-  border-bottom: 1px solid ${color.gray};
-`;
+const SubTitleContainer = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+  marginBottom: '16px',
+});
 
 function PostDetails({
   title,
@@ -71,7 +40,8 @@ function PostDetails({
   content,
   date,
   image,
-}: PostDetailsProps) {
+  tags,
+}: FrontMatter) {
   return (
     <Main
       meta={
@@ -79,37 +49,71 @@ function PostDetails({
           title={title}
           description={description}
           post={{
-            image: image,
+            image: image ?? '',
             date: date,
-            modified_date: modified_date,
+            modified_date: modified_date ?? '',
           }}
         />
       }>
       <PostDetailsLayout>
         <TitleContainer>
-          <div className="title">{title}</div>
-          <div className="date" style={{ marginTop: 15 }}>
+          <Typography variant="subtitle1" color="textSecondary" mb="8px">
             {format(new Date(date), 'LLLL d, yyyy')}
-          </div>
+          </Typography>
+          <Typography variant="h3" fontWeight="700">
+            {title}
+          </Typography>
         </TitleContainer>
         <SubTitleContainer>
-          <div className="author-img" />
-          <span className="author-name">{Config.author}</span>
+          <Avatar
+            alt="Min Byeong Chan Profile Icon"
+            src="/assets/images/mbc_img.png"
+            sx={{ width: 48, height: 48 }}
+          />
+          <Box>
+            <Typography variant="subtitle1" fontWeight="700">
+              {Config.author}
+            </Typography>
+            <Box display="flex" gap="8px">
+              <Tooltip title={Config.instagram}>
+                <Icon sx={{ width: '24px', height: '24px' }}>
+                  <Link href={Config.instagram}>
+                    <Instagram />
+                  </Link>
+                </Icon>
+              </Tooltip>
+              <Tooltip title={Config.email}>
+                <Icon sx={{ width: '24px', height: '24px' }}>
+                  <Link href={'mailto:' + Config.email}>
+                    <Email />
+                  </Link>
+                </Icon>
+              </Tooltip>
+            </Box>
+          </Box>
         </SubTitleContainer>
+
+        <SubTitleContainer>
+          <Typography fontWeight="700">TAGS</Typography>
+          <Box display="flex" gap="16px">
+            {tags.map((tag) => (
+              <Typography key={tag} color={Color.LightPink}>
+                {tag}
+              </Typography>
+            ))}
+          </Box>
+        </SubTitleContainer>
+
+        <Divider />
 
         <Markdown content={content} />
 
-        {/* 댓글 컨테이너 - 시작 */}
-        <CommentTitleContainer>
-          <TextDefault size="xg" weight={fontWeight.bold}>
-            Comment
-          </TextDefault>
-        </CommentTitleContainer>
-        {/* 댓글 컨테이너 - 시작 */}
+        <Typography variant="h5" fontWeight="700" margin="50px 0 20px 0">
+          Comment
+        </Typography>
+        <Divider />
 
-        {/* Utterances - 시작 */}
         <Utterances repo="MinByeongChan/mbc-devBlog" theme="github-light" />
-        {/* Utterances - 끝 */}
       </PostDetailsLayout>
     </Main>
   );
@@ -128,18 +132,11 @@ export const getStaticPaths: GetStaticPaths<PostUrl> = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps<PostDetailsProps, PostUrl> = async ({ params }) => {
+export const getStaticProps: GetStaticProps<FrontMatter, PostUrl> = async ({ params }) => {
   const post = getPostBySlug(params!.slug);
 
   return {
-    props: {
-      title: post.title,
-      description: post.description !== undefined ? post.description : '',
-      date: post.date,
-      modified_date: post.modified_date !== undefined ? post.modified_date : '',
-      image: post.image !== undefined ? post.image : '',
-      content: post.content || '',
-    },
+    props: { ...post },
   };
 };
 
