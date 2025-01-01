a1a109400fa4130689998c9c7aae34eb79d3fdb9
cw-mbc0481 <mbc0481@coway.com>
2025-01-01 22:54:54 +0900

    feat(25): tag filter 기능 개발

--- src/components/blog/BlogMain/BlogMain.tsx
+++ src/components/blog/BlogMain/BlogMain.tsx
@@ -3,6 +3,7 @@ import { ArticleContainer, LnbContainer } from '@/components/blog/BlogMain/BlogM
 import { LnbList } from '@/components/blog/LnbList/LnbList';
 import { BlogListItem, FrontMatter } from '@/types';
 import { getTagItems } from '@/utils/utils';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
 interface BlogMainProps {
@@ -10,14 +11,23 @@ interface BlogMainProps {
 }
 
 export const BlogMain = ({ totalPostList }: BlogMainProps) => {
+  const searchParams = useSearchParams();
+  const tag = searchParams.get('tag') || '';
+
   const tagItems = getTagItems(totalPostList);
-  const blogListItem = totalPostList.map<BlogListItem>((data) => ({
-    id: data.slug,
-    date: data.date,
-    title: data.title,
-    tags: data.tags,
-    description: data.description || data.content.substring(0, 200),
-  }));
+  const blogListItem = totalPostList
+    .map<BlogListItem>((data) => ({
+      id: data.slug,
+      date: data.date,
+      title: data.title,
+      tags: data.tags,
+      description: data.description || data.content.substring(0, 200),
+    }))
+    .filter((data) => {
+      if (!tag) return true;
+      const lowerCaseList = data.tags?.map((data) => data.toLowerCase()) ?? [];
+      return lowerCaseList.includes(tag.toLowerCase());
+    });
 
   return (
     <>
